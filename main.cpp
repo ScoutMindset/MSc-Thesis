701876b07ca3bbad3804d965e1a6fcaa7fd04c04
unknown <jaturul@gmail.com>
2015-03-10 22:38:12 +0100

    This is the first version of the camera displaying program.

---
+++ main.cpp
@@ -0,0 +1,59 @@
+/** 
+* @author: Jacek Turula
+* This program is suppoused to display live video from the camera.
+*/
+
+#include <iostream>
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/video/tracking.hpp>
+
+using namespace std;
+using namespace cv;
+
+//Mat detectMotion(Mat& frame1, Mat& frame2);
+
+int main()
+{
+	VideoCapture capture;
+	capture.open(0);
+	Mat frame;
+	namedWindow("Video");
+
+	while (1)
+	{
+		capture >> frame;
+		if (!(frame.empty())){
+			imshow("Video", frame);
+		}
+		waitKey(10);
+		//cout << frame.rows << " " << frame.cols << " " << capture.isOpened() << std::endl;
+		
+	}
+}
+
+//Mat detectMotion(Mat& frame1, Mat& frame2)
+//{
+//	Mat frame1Gray, frame2Gray;
+//	int rows = frame1.rows;
+//	int cols = frame2.cols;
+//	int difference;
+//	Mat motionMap(rows, cols, CV_8UC1);
+//
+//	cvtColor(frame1, frame1Gray, CV_RGB2GRAY);
+//	cvtColor(frame2, frame2Gray, CV_RGB2GRAY);
+//
+//	for (int i = 0; i < rows; i++)
+//	{
+//		for (int j = 0; j < cols; j++)
+//		{
+//			difference = abs(frame2Gray.at<uchar>(i, j) - frame1Gray.at<uchar>(i, j));
+//			if (difference > 5)
+//				motionMap.at<uchar>(i, j) = 255;
+//			else
+//				motionMap.at<uchar>(i, j) = 0;
+//		}
+//	}
+//	return motionMap;
+//}
