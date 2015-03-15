f27c68b1beb4b8700817ce5afe5267b437b00da9
unknown <jaturul@gmail.com>
2015-03-15 13:49:35 +0100

    This is the display&motion detection with the frame-capturing interface done using "if" functions

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@
 
 #include <iostream>
 
+#include <windows.h>
+
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/video/tracking.hpp>
@@ -28,6 +30,8 @@ Mat detectMotion(Mat& frame1, Mat& frame2, int threshold);
 int main()
 {
 	/// VARIABLES
+	HWND consoleWindow = GetConsoleWindow();
+	MoveWindow(consoleWindow, 10, 10, 800, 480, FALSE);
 	VideoCapture capture; /// This variable captures the video stream from the camera.
 	capture.open(0);	/// The video stream variable is initialized with the default ('0') camera device on the PC.
 
@@ -38,27 +42,93 @@ int main()
 	Mat motionMap; /// Image variable that is the result of image detection.
 
 	int threshold = 15;
+	int delay = 0;
+	int k = -1;
+
+	signed char input;
 
 	/// INITIAL CAMERA OPERATIONS
 	namedWindow("Video");
+	moveWindow("Video", 10, 500);
 	namedWindow("Difference Image");
+	moveWindow("Difference Image", 700, 500);
 	capture >> frame;
+	flip(frame, frame, 1); /// This function causes the mirror-like display of the video from the camera.
 	prevFrame = frame.clone(); /// The previous frame is initialized with the current variable before entering the while loop.
 	waitKey(10);  /// A wait is performed so that the camera has enough time to start working.
 
 	/// PROCESSING LOOP
+	cout << "Press ESCAPE in order to leave the program." << endl;
 	while (1)
 	{
-		capture >> frame; /// Current frame is captured and displayed. WARNING: THIS APPROACH CAUSES THE FIRST VIDEO FRAME TO NOT BE DISPLAYED!
+		capture >> frame; /// Current frame is captured and displayed. WARNING: THIS APPROACH CAUSES THE FIRST VIDEO FRAME TO NOT BE DISPLAYED
+						  /// BECAUSE THE FIRST FRAME IS CAPTURED OUTSIDE OF THE WHILE LOOP!
 		if (!(frame.empty())) /// The operations are conducted only if the captured video frame is not empty!
 		{
+			flip(frame, frame, 1);
 			imshow("Video", frame);
-			motionMap = detectMotion(prevFrame, frame,threshold);
+			/// MOTION DETECTION BEGINS
+			motionMap = detectMotion(prevFrame, frame, threshold);
 			imshow("Difference Image", motionMap);
 			prevFrame = frame.clone(); /// Current frame is assigned as the previous frame for the next iteration of the 'while' loop.
-			waitKey(10);
+
+			/// INTERFACE OF FRAME-CAPTURING 
+			/**
+			* k == -1 (the program invites the user to capture the video frame without his/her body)
+			* k == 0  (the program captures the frame without the user's body)
+			* k == 1  (the program invites the user to capture the video frame with his/her body)
+			* k == 2  (the program captures the frame with the user's body)
+			* k == 3  (the program has captured all needed frames and continues with other operations
+			*/
+
+			if (k == -1)
+			{
+				cout << "Please ENTER to capture the frame WITHOUT the body of the player." << endl;
+				k = 0;
+			}
+			else if (k == 1)
+			{
+				cout << "Please ENTER to capture the frame WITH the body of the player." << endl;
+				k = 2;
+			}
+
+			input = waitKey(30); /// \var input This variable determines whether to capture the frame ("ENTER") or leave the program ("ESCAPE")
+
+			if ( (input == 13) && (k == 0) )
+			{
+				delay = 50; /// The first instance of ENTER-pressing causes the activation of frame-capturing delay so that the player can leave
+							/// the frame so that frame WITHOUT the player's body is captured.
+
+			}
+			else if ( (input == 13) && (k == 2) )
+			{
+				templatePlayer = frame;
+				namedWindow("Player template");
+				moveWindow("Player template", 920, 10);
+				imshow("Player template", frame);
+				k = 3;
+			}
+			else if (input == 27)
+				break;
+
+			if (delay != 0) /// This part of the code is responsible for the countdown that allows the user to remove her/himself from the frame.
+							/// WARNING: THIS COUNTDOWN IS NOT IN SECONDS AND DEPENDS ON HOW FAST THE REST OF THE WHILE LOOP IS EXECUTED!
+			{
+				if ((delay % 10) == 0 )
+					cout << delay/10 << endl;
+				delay--;
+				if (delay == 0)
+				{
+					templateEmpty = frame;
+					k = 1;
+					namedWindow("Empty template");
+					moveWindow("Empty template", 900, 10);
+					imshow("Empty template", frame);
+				}
+			}
 		}
 	}
+	return 0;
 }
 
 Mat detectMotion(Mat& frame1, Mat& frame2, int threshold)
