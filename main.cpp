fa03971f6724c49fd6c512385826f583f8f55395
unknown <jaturul@gmail.com>
2015-03-15 12:21:09 +0100

    This is the documented version of the program that displays camera image and computes the motion map using differential image.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 /** 
 * @author: Jacek Turula
+* @file main.cpp
 * This program is suppoused to display live video from the camera.
 */
 
@@ -12,24 +13,37 @@
 using namespace std;
 using namespace cv;
 
-Mat detectMotion(Mat& frame1, Mat& frame2);
+/**
+* \fn Mat detectMotion(Mat& frame1, Mat& frame2, int threshold);
+* \param frame 1 is the previous video frame
+* \param frame 2 is the current video frame.
+* \param threshold is the value to which the difference between the frames is compared.
+* \return the returned variable is a video frame containing the motion map.
+* \brief This function computes the motion map for the previous (frame1) and current (frame2) video frames.
+* The difference between the two frames is computed and if it is greater than the specified threshold, the value of that pixel of the motion map
+* is set to 255. Otherwise, it's set to 0.
+*/
+Mat detectMotion(Mat& frame1, Mat& frame2, int threshold);
 
 int main()
 {
-	VideoCapture capture;
-	capture.open(0);
-	Mat frame, prevFrame, motionMap;
+	VideoCapture capture; /// This variable captures the video stream from the camera.
+	capture.open(0);	/// The video stream variable is initialized with the default ('0') camera device on the PC.
+	Mat frame; ///Current frame image variable.
+	Mat prevFrame; 
+	Mat motionMap; /// Image variable that is the result of image detection.
+	int threshold = 15;
 	namedWindow("Video");
 	namedWindow("Difference Image");
 	capture >> frame;
-	prevFrame = frame.clone();
-	waitKey(10);
+	prevFrame = frame.clone(); /// The previous frame is initialized with the current variable before entering the while loop.
+	waitKey(10);  /// A wait is performed so that the camera has enough time to start working.
 	while (1)
 	{
 		capture >> frame;
 		if (!(frame.empty())){
 			imshow("Video", frame);
-			motionMap = detectMotion(prevFrame, frame);
+			motionMap = detectMotion(prevFrame, frame,threshold);
 			imshow("Difference Image", motionMap);
 		}
 		prevFrame = frame.clone();
@@ -39,15 +53,18 @@ int main()
 	}
 }
 
-Mat detectMotion(Mat& frame1, Mat& frame2)
+Mat detectMotion(Mat& frame1, Mat& frame2, int threshold)
 {
 	Mat frame1Gray, frame2Gray;
 	int rows = frame1.rows;
 	int cols = frame2.cols;
 	int difference;
-	Mat motionMap(rows, cols, CV_8UC1);
 
-	cvtColor(frame1, frame1Gray, CV_RGB2GRAY);
+	Mat motionMap(rows, cols, CV_8UC1); /// The difference image is initialized with the same amount of rows and columns as the function's frames.
+										/// 
+
+	cvtColor(frame1, frame1Gray, CV_RGB2GRAY); /// Both previous and current frames are converted to the Grayscale color space so that the
+											   /// computation of differential image is faster.
 	cvtColor(frame2, frame2Gray, CV_RGB2GRAY);
 
 	for (int i = 0; i < rows; i++)
@@ -55,7 +72,7 @@ Mat detectMotion(Mat& frame1, Mat& frame2)
 		for (int j = 0; j < cols; j++)
 		{
 			difference = abs(frame2Gray.at<uchar>(i, j) - frame1Gray.at<uchar>(i, j));
-			if (difference > 15)
+			if (difference > threshold)
 				motionMap.at<uchar>(i, j) = 255;
 			else
 				motionMap.at<uchar>(i, j) = 0;
